fbf018aa645c050ea8afe363f715cd201fade875
aulauniversal <168633732+aulauniversal@users.noreply.github.com>
2024-05-06 21:42:06 +0200

    Create AutoinstallWin.bat

---
+++ AutoinstallWin.bat
@@ -0,0 +1,22 @@
+@echo off
+REM AULAUNIVERSAL 2024
+REM Instalación de Python
+echo Instalando Python...
+start /w "" https://www.python.org/ftp/python/3.10.0/python-3.10.0-amd64.exe
+REM Asegúrate de seleccionar la opción "Agregar Python 3.x al PATH" durante la instalación.
+
+REM Instalación de Npcap
+echo Instalando Npcap...
+start /w "" npcap-1-79.exe
+REM Sigue las instrucciones de instalación de Npcap.
+
+REM Instalación de Scapy
+echo Instalando Scapy...
+pip install scapy
+
+REM Instalación de otras dependencias de Python
+echo Instalando otras dependencias de Python...
+pip install -r requirements.txt
+
+echo Instalación completada.
+pause
